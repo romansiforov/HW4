11bf8bb4842559436eb0661b59d53a1e49a2347a
Roman <roman@defentry.com>
2023-05-02 17:06:18 +0300

    HW 4 complted. Last task implementation process kilometers.

---
+++ 8-9 tasks string if else.js
@@ -0,0 +1,32 @@
+// 8.*  Використовуючи властивості рядків (тип string), та регулярний вираз (regular expression) видалити голосні букви зі слова.
+
+let regExpGolosny = "AEIOUYaeiouy";
+
+let testWord = "Synchronization"; 
+
+console.log("The word BEFORE regExp applying---------- "+testWord);
+testWord = testWord.replace(/[AEIOUYaeiouy]/g, "")
+console.log("The word After regExp applying ---------- "+testWord);
+
+
+// 9.*  Використовуючи оператор if реалізувати логіку переводу метрів в кілометри,
+//     так щоб в консоль виводився результат обчислень з правильним закінченням.
+//     Наприклад: 1000 метрів це 1 кілометр;  32 метри це 0,032 кілометра і т.д.
+//     Підказка(https://ukr-lifehacks.ed-era.com/rozdil-9/zvyazok_chislivnykiv)
+
+// 1 km == 1000m
+
+let metersAmount = 22000; 
+let kilometersAmount;
+let kilometr = "кiлометр";
+
+kilometersAmount = metersAmount/1000;
+
+if (String(kilometersAmount).match(/0$/) || String(kilometersAmount).match(/[1][1-9]$/) || String(kilometersAmount).match(/[2-9][5-9]$/)){
+    console.log(metersAmount+" метр(а) це "+kilometersAmount+" "+kilometr+"iв");
+
+} else if(String(kilometersAmount).match(/1$/)){
+    console.log(metersAmount+" метр(а) це "+kilometersAmount+" "+kilometr+"");
+} else if(String(kilometersAmount).match(/[2-4]$/) && !String(kilometersAmount).match(/[1][1-9]$/)){
+    console.log(metersAmount+" метр(а) це "+kilometersAmount+" "+kilometr+"a");
+}
